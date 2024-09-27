0322d69cbad37d7ac87a08f912e94f46c4df43a1
BrunoTSantosNeves <tonetti583@gmail.com>
2024-09-27 18:27:45 -0300

    Finalizando o arquivo board1.h, que possui funções que manipulam a matriz, lendo valores, atualizando e iniciando a mesma

---
+++ board1.h
@@ -0,0 +1,14 @@
+#ifndef BOARD1_H
+#define BOARD1_H
+
+void	map_get_section2(struct s_board_t *board, unsigned int round, unsigned int *values);
+
+void	map_get_section(struct s_board_t *board, unsigned int round, unsigned int *values);
+
+void	map_update_section2(struct s_board_t *board, unsigned int round, unsigned int *values);
+
+void	map_update_section(struct s_board_t *board, unsigned int round, unsigned int *values);
+
+void	map_init(struct s_board_t *board);
+
+#endif
