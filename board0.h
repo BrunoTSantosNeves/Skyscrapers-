7892fa501448fe536e215504060e14885c728b7c
BrunoTSantosNeves <tonetti583@gmail.com>
2024-09-28 12:04:16 -0300

    board0.h Este arquivo de cabeçalho define quatro funções relacionadas a manipulações de tabuleiro (board) e resoluções (resolutions)

---
+++ board0.h
@@ -0,0 +1,12 @@
+#ifndef BOARD0_H
+#define BOARD0_H
+
+struct s_board_t	*map_clone(struct s_board_t *src);
+
+void	board_show(struct s_board_t *board);
+
+void	resolutions_init(struct s_resolutions_t *resolutions);
+
+void	resolutions_append(struct s_resolutions_t *resolutions, struct s_board_t *board);
+
+#endif
---
+++ struct.c
@@ -0,0 +1,4 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include "struct.h"
+#include "
