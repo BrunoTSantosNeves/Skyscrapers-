8547a6f71f70771466a2ecdf1a19dc7bdc6dca63
BrunoTSantosNeves <tonetti583@gmail.com>
2024-09-28 12:26:10 -0300

    Desenvolvendo o arquivo struct.c

--- struct.c
+++ struct.c
@@ -1,4 +1,32 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "struct.h"
-#include "
+#include "board0.h"
+#include "board1.h"
+#include "evaluate.h"
+
+const char	*get_string(unsigned int value)
+{
+	if (value == 0)
+	{
+		return("0");
+	}
+	else if (value == 1)
+	{
+		return("1");
+	}
+	else if (value == 2)
+	{
+		return("2");
+	}
+	else if (value == 3)
+	{
+		return("3");
+	}
+	else if (value == 4)
+	{
+		return("4");
+	}
+	else
+		return("U");
+}
