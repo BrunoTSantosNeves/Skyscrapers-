dc88ed95dcb22855c7f991d00073882b9a8ceaea
BrunoTSantosNeves <tonetti583@gmail.com>
2024-10-03 17:34:00 -0300

    função map_get_section2 feita no arquivo board1.c, essa função tem o objetivo de deixar os números visiveis dentro das linhas da matriz, tanto da direita para a esquerda quanto da esquerda para a direita

--- board1.c
+++ board1.c
@@ -10,4 +10,16 @@ void	map_get_section2(struct s_board_t *board, unsigned int round, unsigned int 
 	if (round < 12)
 	{
 		values[0] = (*board).values[round - 8][0];
-		values[1] = (*board).values[round - 8][1];'
+		values[1] = (*board).values[round - 8][1];
+		values[2] = (*board).values[round - 8][2];
+		values[3] = (*board).values[round - 8][3];
+	}
+	else
+	{
+		values[0] = (*board).values[round - 12][3];
+		values[1] = (*board).values[round - 12][2];
+		values[2] = (*board).values[round - 12][1];
+		values[3] = (*board).values[round - 12][0];
+	}
+}
+
